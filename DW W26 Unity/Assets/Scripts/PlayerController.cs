7ee428fefaf5f6a39a8f8a0147f574c6f13f9461
Raphaël Tétreault <41877613+RaphaelTetreault@users.noreply.github.com>
2026-02-08 18:38:42 -0500

    Update aspect ratio and base geometry

--- DW W26 Unity/Assets/Scripts/PlayerController.cs
+++ DW W26 Unity/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,34 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     [field: SerializeField] public int PlayerNumber { get; private set; }
+    [field: SerializeField] public Color PlayerColor { get; private set; }
+    [field: SerializeField] public SpriteRenderer SpriteRenderer { get; private set; }
     [field: SerializeField] public Rigidbody2D Rigidbody2D { get; private set; }
-    [field: SerializeField] public float MoveSpeed { get; private set; } = 5f;
+    [field: SerializeField] public float MoveSpeed { get; private set; } = 10f;
     [field: SerializeField] public float JumpForce { get; private set; } = 5f;
 
+    public bool DoJump { get; private set; }
+
+    // Player input information
     private PlayerInput PlayerInput;
     private InputAction InputActionMove;
     private InputAction InputActionJump;
 
-    public void InitializePlayer(PlayerInput playerInput)
+    // Assign color value on spawn from main spawner
+    public void AssignColor(Color color)
+    {
+        // record color
+        PlayerColor = color;
+
+        // Assign to sprite renderer
+        if (SpriteRenderer == null)
+            Debug.Log($"Failed to set color to {name} {nameof(PlayerController)}.");
+        else
+            SpriteRenderer.color = color;
+    }
+
+    // Set up player input
+    public void AssignPlayerInputDevice(PlayerInput playerInput)
     {
         // Record our player input (ie controller).
         PlayerInput = playerInput;
@@ -28,48 +47,28 @@ public class PlayerController : MonoBehaviour
         this.PlayerNumber = playerNumber;
     }
 
+    // Runs each frame
     public void Update()
     {
-        //var InputActionMove = InputSystem.actions.FindAction($"Player/Move");
-        //var x = Gamepad.all[]
-    }
-
-
-    // This function gets called because the action map "Player" is selected in the PlayerInput script
-    // and because the action "Move" is defined in the Player action map.
-    public void OnMove(InputValue value)
-    {
-        // Read the "Move" action value, which is a 2D vector
-        Vector2 moveValue = value.Get<Vector2>();
-        // Here we're only using the X axis to move.
-        float moveForce = moveValue.x * MoveSpeed;
-        // Apply fraction of force each frame
-        Rigidbody2D.AddForceX(moveForce, ForceMode2D.Force);
-
-
-    }
-
-    public void OnJump(InputValue button)
-    {
-        // Get button pressed
-        //bool isPressed = button.performed;
-        //bool isReleased = button.canca;
-        //bool isDown = button.started;
-
-        bool isDown = InputActionJump.IsPressed();
-        bool isUp = !InputActionJump.IsPressed();
-        bool isPressed = InputActionJump.WasPressedThisFrame();
-        bool isReleased = InputActionJump.WasReleasedThisFrame();
-
-        if (isPressed)
+        // Read the "Jump" action state, which is a boolean value
+        if (InputActionJump.WasPressedThisFrame())
         {
-            // Apply all force immediately
-            Rigidbody2D.AddForceY(JumpForce, ForceMode2D.Impulse);
+            // Buffer input becuase I'm controlling the Rigidbody through FixedUpdate
+            // and checking there we can miss inputs.
+            DoJump = true;
         }
     }
 
+    // Runs each phsyics update
     void FixedUpdate()
     {
+        if (Rigidbody2D == null)
+        {
+            Debug.Log($"{name}'s {nameof(PlayerController)}.{nameof(Rigidbody2D)} is null.");
+            return;
+        }
+
+        // MOVE
         // Read the "Move" action value, which is a 2D vector
         Vector2 moveValue = InputActionMove.ReadValue<Vector2>();
         // Here we're only using the X axis to move.
@@ -77,22 +76,28 @@ public class PlayerController : MonoBehaviour
         // Apply fraction of force each frame
         Rigidbody2D.AddForceX(moveForce, ForceMode2D.Force);
 
-        // Read the "Jump" action state, which is a boolean value
-        if (InputActionJump.WasPressedThisFrame())
+        // JUMP - review Update()
+        if (DoJump)
         {
             // Apply all force immediately
             Rigidbody2D.AddForceY(JumpForce, ForceMode2D.Impulse);
+            DoJump = false;
         }
     }
 
+    // OnValidate runs after any change in the inspector for this script.
     private void OnValidate()
     {
         Reset();
     }
 
+    // Reset runs when a script is created and when a script is reset from the inspector.
     private void Reset()
     {
+        // Get if null
         if (Rigidbody2D == null)
             Rigidbody2D = GetComponent<Rigidbody2D>();
+        if (SpriteRenderer == null)
+            SpriteRenderer = GetComponent<SpriteRenderer>();
     }
 }
